b50625ff05ef681bc7cdff97e8d4e2f3d1cc3842
vitalii-panama <js.patriarch@protonmail.com>
2024-08-26 18:03:18 +0300

    init

--- tailwind.config.js
+++ tailwind.config.js
@@ -1,4 +1,5 @@
-const path = require("path");
+const path = require("path")
+const plugin = require("tailwindcss/plugin")
 
 module.exports = {
   theme: {
@@ -7,13 +8,30 @@ module.exports = {
       padding: "1rem",
     },
     extend: {
+      fontSize: {
+        "heading1": "66px",
+      },
       fontFamily: {
         "adorn-condensed-sans": ["Adorn Condensed Sans", "sans-serif"],
+      },
+      textShadow: {
+        DEFAULT: "1px 2px #000",
       },
     },
   },
 
-  plugins: [],
+  plugins: [
+    plugin(function ({ matchUtilities, theme }) {
+      matchUtilities(
+        {
+          "text-shadow": (value) => ({
+            textShadow: value,
+          }),
+        },
+        { values: theme("textShadow") }
+      )
+    }),
+  ],
   content: [
     path.resolve(__dirname, "./src/**/*.{js,vue}"),
     path.resolve(__dirname, "./shopify/**/*.liquid"),
